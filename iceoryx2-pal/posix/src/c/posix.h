9006bd3e0b75f9617f4caeed79e9f5b8df381118
Kelli Schwalm <kelli@runsafesecurity.com>
2025-01-03 15:56:10 -0600

    remove sigaction and readd signal.h

--- iceoryx2-pal/posix/src/c/posix.h
+++ iceoryx2-pal/posix/src/c/posix.h
@@ -56,8 +56,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
+#include <signal.h>
 #include <bits/signum.h>
-#include <bits/sigaction.h>
 
 #if !(defined(_WIN64) || defined(_WIN32))
 struct iox2_sigaction {
